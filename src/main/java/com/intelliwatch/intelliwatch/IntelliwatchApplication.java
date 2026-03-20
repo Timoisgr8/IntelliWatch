d1a455e565871720a43191f4836975922d7df907
Timothy Tew <tewtimothy@gmail.com>
2026-03-20 12:30:09 +1030

    Spring Boot

---
+++ src/main/java/com/intelliwatch/intelliwatch/IntelliwatchApplication.java
@@ -0,0 +1,13 @@
+package com.intelliwatch.intelliwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intelliwatch/intelliwatch/IntelliwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelliwatch.intelliwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
